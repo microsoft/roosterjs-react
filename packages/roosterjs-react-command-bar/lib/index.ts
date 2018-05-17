94226294de6004e9c3cdf9113d08cc784bf593a6
Adrian Tran <adtran@microsoft.com>
2018-05-17 13:21:46 -0700

    reverted index.ts

--- packages/roosterjs-react-command-bar/lib/index.ts
+++ packages/roosterjs-react-command-bar/lib/index.ts
@@ -1,5 +1,5 @@
 export { default as RoosterCommandBar } from './components/RoosterCommandBar';
-export { RoosterCommandBarProps, RoosterCommandBarButton } from './schema/RoosterCommandBarSchema';
+export { RoosterCommandBarProps, RoosterCommandBarState, RoosterCommandBarButton } from './schema/RoosterCommandBarSchema';
 export { default as RoosterCommandBarPlugin } from './plugins/RoosterCommandBarPlugin';
 export { default as RoosterCommandBarPluginInterface } from './schema/RoosterCommandBarPluginInterface';
 export { RoosterCommandBarStringKeys, RoosterCommmandBarButtonKeys } from './utils/OutOfBoxCommandBarButtons';
