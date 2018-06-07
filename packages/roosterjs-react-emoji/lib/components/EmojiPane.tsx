f5638f1d40015cc444c170e35bf8d6adce70f4ae
Adrian Tran <adtran@microsoft.com>
2018-06-06 21:53:59 -0700

    Target latest roosterjs and fix build issues for last checkin

--- packages/roosterjs-react-emoji/lib/components/EmojiPane.tsx
+++ packages/roosterjs-react-emoji/lib/components/EmojiPane.tsx
@@ -22,7 +22,6 @@ export interface EmojiPaneState {
 }
 
 export interface EmojiPaneProps {
-    onSelect: (emoji: Emoji, wordBeforeCursor: string) => void;
     strings: Strings;
     quickPickerClassName?: string;
     fullPickerClassName?: string;
@@ -31,13 +30,17 @@ export interface EmojiPaneProps {
     partialListClassName?: string;
 }
 
-export default class EmojiPane extends React.Component<EmojiPaneProps, EmojiPaneState> {
+export interface InternalEmojiPaneProps extends EmojiPaneProps {
+    onSelect: (emoji: Emoji, wordBeforeCursor: string) => void;
+}
+
+export default class EmojiPane extends React.Component<InternalEmojiPaneProps, EmojiPaneState> {
     private static IdCounter = 0;
 
     private baseId = EmojiPane.IdCounter++;
     private searchBox: TextField;
 
-    constructor(props: EmojiPaneProps) {
+    constructor(props: InternalEmojiPaneProps) {
         super(props);
 
         this.state = {
