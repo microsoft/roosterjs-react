1ff57569ea7302294591e63f78924539707e013c
Adrian Tran <adtran@microsoft.com>
2018-06-07 13:12:53 -0700

    Fixed typo

--- packages/roosterjs-react-command-bar/lib/schema/RoosterCommandBarPluginInterface.ts
+++ packages/roosterjs-react-command-bar/lib/schema/RoosterCommandBarPluginInterface.ts
@@ -3,6 +3,6 @@ import { Editor } from 'roosterjs-editor-core';
 
 export default interface RoosterCommandBarPluginInterface {
     getEditor: () => Editor;
-    registerRoosterCommandBar: (ribbon: RoosterCommandBar) => void;
-    unregisterRoosterCommandBar: (ribbon: RoosterCommandBar) => void;
+    registerRoosterCommandBar: (commandBar: RoosterCommandBar) => void;
+    unregisterRoosterCommandBar: (commandBar: RoosterCommandBar) => void;
 }
