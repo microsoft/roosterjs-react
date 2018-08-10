cc893d948575ad051a8b4083ca3907eba72fe94c
Borah Chong <bochong@microsoft.com>
2018-08-09 17:24:59 -0700

    removed commented out code

---
+++ packages/roosterjs-react-emoji/lib/components/EmojiNavBar.tsx
@@ -0,0 +1,38 @@
+import * as React from "react";
+import { TooltipHost } from "office-ui-fabric-react/lib/Tooltip";
+import { Icon } from "office-ui-fabric-react/lib/Icon";
+import EmojiList, { EmojiFabricIconCharacterMap } from "../utils/emojiList";
+import * as EmojiNavBarStyles from "./EmojiNavBar.scss.g";
+import { css } from "roosterjs-react-common";
+
+export interface EmojiNavBarProps {
+    onClick?: (selected: string) => void;
+    currentSelected?: string;
+}
+
+export default class EmojiNavBar extends React.Component<EmojiNavBarProps, {}> {
+    public render() {
+        const { onClick, currentSelected } = this.props;
+
+        return (
+            // for each emoji family key, create a button to use as nav bar
+            <div className={EmojiNavBarStyles.navBar}>
+                {Object.keys(EmojiList).map((key, index) => {
+                    const selectedClassName = key === currentSelected ? EmojiNavBarStyles.selected : "";
+
+                    return (
+                        <TooltipHost hostClassName={EmojiNavBarStyles.navBarTooltip} content={key} key={key}>
+                            <button
+                                className={css(EmojiNavBarStyles.navBarButton, selectedClassName)}
+                                key={key}
+                                onClick={() => onClick(key)}
+                            >
+                                <Icon iconName={EmojiFabricIconCharacterMap[key]} />
+                            </button>
+                        </TooltipHost>
+                    );
+                })}
+            </div>
+        );
+    }
+}
