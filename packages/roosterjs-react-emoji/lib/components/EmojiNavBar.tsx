7b6bd4f285978844ddb307ba189a460e4c805b19
Borah Chong <bochong@microsoft.com>
2018-08-10 13:47:43 -0700

    edit navbar onclick function

--- packages/roosterjs-react-emoji/lib/components/EmojiNavBar.tsx
+++ packages/roosterjs-react-emoji/lib/components/EmojiNavBar.tsx
@@ -4,7 +4,7 @@ import { Icon } from "office-ui-fabric-react/lib/Icon";
 import EmojiList, { EmojiFabricIconCharacterMap } from "../utils/emojiList";
 import * as EmojiNavBarStyles from "./EmojiNavBar.scss.g";
 import { css } from "roosterjs-react-common";
-import { FocusZone } from "office-ui-fabric-react/lib/FocusZone";
+import { FocusZone, FocusZoneDirection } from "office-ui-fabric-react/lib/FocusZone";
 
 export interface EmojiNavBarProps {
     onClick?: (selected: string) => void;
@@ -13,12 +13,12 @@ export interface EmojiNavBarProps {
 
 export default class EmojiNavBar extends React.Component<EmojiNavBarProps, {}> {
     public render() {
-        const { onClick, currentSelected } = this.props;
+        const { currentSelected } = this.props;
 
         return (
             // for each emoji family key, create a button to use as nav bar
             <div className={EmojiNavBarStyles.navBar}>
-                <FocusZone>
+                <FocusZone direction={FocusZoneDirection.horizontal}>
                     {Object.keys(EmojiList).map((key, index) => {
                         const selectedClassName = key === currentSelected ? EmojiNavBarStyles.selected : "";
 
@@ -27,7 +27,7 @@ export default class EmojiNavBar extends React.Component<EmojiNavBarProps, {}> {
                                 <button
                                     className={css(EmojiNavBarStyles.navBarButton, selectedClassName)}
                                     key={key}
-                                    onClick={() => onClick(key)}
+                                    onClick={this.onFamilyClick.bind(this, key)}
                                 >
                                     <Icon iconName={EmojiFabricIconCharacterMap[key]} />
                                 </button>
@@ -37,5 +37,11 @@ export default class EmojiNavBar extends React.Component<EmojiNavBarProps, {}> {
                 </FocusZone>
             </div>
         );
+    }
+
+    private onFamilyClick(key: string): void {
+        if (this.props.onClick) {
+            this.props.onClick(key);
+        }
     }
 }
